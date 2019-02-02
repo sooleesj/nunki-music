9a5e3ca088f9934848847b7f65d6f18f1c1dff5f
semmaow <semmaow@gmail.com>
2019-02-01 17:02:34 -0700

    most recent

--- app.js
+++ app.js
@@ -6,8 +6,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+// used for google upload example (GUE)
+// https://github.com/GoogleCloudPlatform/nodejs-docs-samples/blob/3bb14ef7c23305613bbfe04f03d3b83f6a120e1a/appengine/storage/standard/app.js
+const format = require('util').format;
+const Multer = require('multer');
+const process = require('process');
+
+// not used at the moment
 const request = require('request');
 const rp = require('request-promise');
+const fs = require('fs');
+const http = require('http');
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -17,18 +27,27 @@ app.use(bodyParser.json());
 const {Datastore} = require('@google-cloud/datastore');
 const {Storage} = require('@google-cloud/storage');
 
+// used for GUE
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // no larger than 5mb, change as needed
+  }
+});
 
 
 
 
 // Instantiate a datastore client
 const datastore = new Datastore();
-// create a client (says the demo)
+// create a storage client 
 const storage = new Storage();
-
 
 // name the bucket we're using
 const bucketName = 'nunki-music.appspot.com';
+
+const bucket = storage.bucket(bucketName);
+
 
 async function listFiles(bucketName) {
   // [START storage_list_files]
@@ -56,6 +75,32 @@ const BASEURL = "https://nunki-music.appspot.com";
 
 //-----------------Datastore interaction functions------------------//
 //---Song
+
+//************************************************************
+// Get list of files currently in a particular bucket
+//************************************************************
+
+async function listFiles(bucketName) {
+  // [START storage_list_files]
+
+  // Lists files in the bucket
+  const [files] = await storage.bucket(bucketName).getFiles();
+
+  console.log('Files:');
+  files.forEach(file => {
+    console.log(file.name);
+  });
+  return files
+  // [END storage_list_files]
+}
+
+// Helper Functions
+
+// used to get all info about an entity
+function fromDatastore(item){
+    item.id = item[Datastore.KEY].id;
+    return item;
+}
 
 
 function postSong(song) {
@@ -93,11 +138,51 @@ app.post('/songs', (req, res) => {
   });
 });
 
+//***************************************************************************
+// File Upload
+//***************************************************************************
+
+// begin GUE
+// [START process]
+// Process the file upload and upload to Google Cloud Storage.
+app.post('/upload', multer.single('file'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
+
+  // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    resumable: false
+  });
+
+  blobStream.on('error', (err) => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+    res.status(200).send(publicUrl);
+  });
+
+  blobStream.end(req.file.buffer);
+});
+// [END process]
+// END File Upload
+//***************************************************************************
+
+
+
+//***************************************************************************
+// List all songs in the Bucket
+//***************************************************************************
 // get a list of songs on the server
 app.get('/songs/', (req, res) => {
   const songs = listFiles(bucketName)
     .then((songs) => {
-      console.log(songs)
+      //console.log(songs)
       res
         .status(200)
         .json(songs);
@@ -109,13 +194,28 @@ app.get('/songs/', (req, res) => {
     });
 });
 
+// END List Songs
+//****************************************************************************
+
+
+
+
+
 // Format off -don't know how streaming works, placeholder for now
-app.get('/songs/:songID', (req, res) => {
-  const song = streamSong(req.params.songID)
-    .then((song) => {
+//****************************************************************************
+// Stream a song by ID
+//
+//****************************************************************************
+app.get('/songs/stream/:songName', (req, res) => {
+  //const song = streamSong(req.params.songID)
+  const publicUrl = format
+      ('https://storage.googleapis.com/${bucket.name}/${req.params.songName}');
+  const file = fs.createWriteStream("song.mp3");
+
+  http.get(publicUrl, response => {
       res
         .status(200)
-        .json(song);
+        .pipe(file);
     }).catch(function(error) {
       if (error.name == 'InvalidSongIDError') {
         res
